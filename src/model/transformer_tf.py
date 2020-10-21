e52d5030a4c4b6863a7b0620e2cfe717575e449d
jiangxiluning <jiangxiluning@gmail.com>
2020-10-21 10:52:18 +0800

    remove duplicated embedding process

--- src/model/transformer_tf.py
+++ src/model/transformer_tf.py
@@ -204,7 +204,7 @@ class Decoder(tf.keras.layers.Layer):
         seq_len = tf.shape(x)[1]
         attention_weights = {}
 
-        x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
+        # x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
         x += self.pos_encoding[:, :seq_len, :]
 
